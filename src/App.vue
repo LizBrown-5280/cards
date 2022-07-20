87bc05051faa9b910418ceceaf37c51363859f2b
Liz Brown <liz.brown@entercom.com>
2022-07-20 16:52:12 -0600

    start

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <h1>Card Game</h1>
+  </header>
+
+  <main>
+    Number of cards: <input v-model.number="amount" type="number" />
+    <Cards :amount=amount />
+  </main>
+</template>
+
+<script>
+import Cards from '@/components/Cards.vue';
+
+export default {
+  name: 'Home',
+  components: { Cards },
+    data() {
+    return {
+      amount: 3
+    }
+  }
+}
+</script>
+
+<style>
+  input {
+    width: 45px;
+  }
+</style>
---
+++ src/components/Cards.vue
@@ -0,0 +1,48 @@
+<template>
+  <table>
+    <tr>
+      <td v-for="num in amount" :key="num" @click="updateSelectedCard(num)">{{ selectedCard(num) }}</td>
+    </tr>
+  </table>
+</template>
+
+<script>
+  // import Card from '@/components/Card.vue';
+
+export default {
+  name: 'Cards',
+  props: {
+    amount: Number
+  },
+  data() {
+    return {
+      cards: [],
+      cardSelected: 1,
+    }
+  },
+  methods: {
+    updateSelectedCard(num) {
+      this.cardSelected = num;
+    },
+    selectedCard(num) {
+      return num === this.cardSelected ? 'up' : 'down';
+    }
+  }
+}
+</script>
+
+<style>
+  td {
+    width: 50px;
+    height: 50px;
+    margin: 0 10px;
+    border-radius: 50px;
+    border: 2px solid grey;
+    text-align: center;
+    cursor: pointer;
+  }
+  td:hover {
+    background-color: rgba(255, 255, 255, 0.3);
+  }
+
+</style>
